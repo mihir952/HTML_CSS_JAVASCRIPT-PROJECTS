492a0cd48aa9359478e20597b53f66be5d72b133
Mihir Kumar <mihir.kumar@valtech.com>
2023-06-22 17:42:14 +0530

    manipulation on json data

---
+++ Js_DomManipulation/json.js
@@ -0,0 +1,88 @@
+ const employees=[
+    {
+        "name": "Mariela Wilkinson",
+        "info": {
+            "title": "Dr.",
+            "age": 21,
+            "phone": "(241) 930-8285",
+            "email": "gutkowski.danielle@yahoo.com"
+        }
+    },
+    {
+        "name": "Jakayla Corkery",
+        "info": {
+            "title": "Dr.",
+            "age": 45,
+            "phone": "895.439.6978 x9907",
+            "email": "matilda10@yahoo.com"
+        }
+    },
+    {
+        "name": "Aurelia Wilkinson",
+        "info": {
+            "title": "Ms.",
+            "age": 45,
+            "phone": "731-315-1416",
+            "email": "dameon.carter@gmail.com"
+        }
+    },
+    {
+        "name": "Evie Gerhold",
+        "info": {
+            "title": "Prof.",
+            "age": 57,
+            "phone": "1-546-419-1288",
+            "email": "brenna.conroy@hotmail.com"
+        }
+    },
+    {
+        "name": "Theron Rolfson",
+        "info": {
+            "title": "Dr.",
+            "age": 55,
+            "phone": "763-392-5691 x48955",
+            "email": "gerda34@hotmail.com"
+        }
+    },
+    {
+        "name": "Rahul McKenzie",
+        "info": {
+            "title": "Dr.",
+            "age": 43,
+            "phone": "(263) 835-2433 x38088",
+            "email": "mertz.leland@hotmail.com"
+        }
+    }
+]
+const tablebody=document.getElementById("tdata");
+employees.forEach((employee)=>{
+    const row=document.createElement("tr");
+    const name=document.createElement("td");
+    const title=document.createElement("td");
+    const age=document.createElement("td");
+    const phone=document.createElement("td");
+    const email=document.createElement("td");
+
+
+    name.innerHTML=employee.name;
+    title.textContent=employee.info.title;
+    age.textContent=employee.info.age;
+    phone.textContent=employee.info.phone;
+    email.textContent=employee.info.email;
+   
+    row.appendChild(name);
+    row.appendChild(title);
+    row.appendChild(age);
+    row.appendChild(phone);
+    row.appendChild(email);
+    
+
+    tablebody.appendChild(row);
+
+
+ 
+
+});
+
+
+
